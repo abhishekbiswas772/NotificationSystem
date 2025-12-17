3a3461f89d044d91459434c7e87aed0a4096c429
Abhishek Biswas <abhishek.biswas@cubastion.com>
2025-12-18 02:00:53 +0900

    fix

---
+++ idempotent_check.py
@@ -0,0 +1,10 @@
+
+
+
+class NotificatonService:
+    def __init__(self, redis_client):
+        self.redis = redis_client
+
+
+    def generate_idempotency_key():
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from notificationsystem!")
+
+
+if __name__ == "__main__":
+    main()
